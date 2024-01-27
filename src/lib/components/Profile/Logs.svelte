361facf41ff303cf7e89e8aebe7d28d6449ca32a
gursheyss <rkwarya@gmail.com>
2024-01-27 15:05:23 -0800

    add border colors for critical levels

--- src/lib/components/Profile/Logs.svelte
+++ src/lib/components/Profile/Logs.svelte
@@ -5,7 +5,9 @@
 <section>
 	<h1 class="mb-4 text-2xl font-bold">Medical Logs</h1>
 	{#each data as log (log.id)}
-		<article class="mb-4 flex flex-col rounded-lg bg-white p-4 shadow">
+		<article
+			class={`mb-4 flex flex-col rounded-lg border-2 bg-white p-4 ${log.level === 3 ? 'border-red-500' : log.level === 2 ? 'border-yellow-500' : ''}`}
+		>
 			<h2 class="text-lg font-bold">{log.label} by {log.doctor}</h2>
 			<p class="text-sm text-gray-600">Description: {log.description}</p>
 			<p class="text-sm text-gray-600">
